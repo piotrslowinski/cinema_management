b4d95baf57281457fedf17609de6eb1c07be062b
piotrslowinski <piotrslowinski89@gmail.com>
2018-04-05 18:57:37 +0200

    CreateShows validation

--- src/main/java/pl/com/piotrslowinski/model/commands/CreateShowsCommand.java
+++ src/main/java/pl/com/piotrslowinski/model/commands/CreateShowsCommand.java
@@ -1,8 +1,10 @@
 package pl.com.piotrslowinski.model.commands;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import pl.com.piotrslowinski.model.repositories.CinemaRepository;
 import pl.com.piotrslowinski.model.repositories.MovieRepository;
 
+import java.time.DayOfWeek;
 import java.time.LocalDateTime;
 import java.util.Set;
 
@@ -10,6 +12,7 @@ public class CreateShowsCommand implements Command {
 
     Long movieId, cinemaId;
 
+    @JsonFormat(pattern = "yyyy/MM/dd HH:mm")
     Set<LocalDateTime> dates;
 
     ShowsCalendar calendar;
@@ -44,5 +47,51 @@ public class CreateShowsCommand implements Command {
 
     public void setCalendar(ShowsCalendar calendar) {
         this.calendar = calendar;
+    }
+
+    public void validate(Validatable.ValidationErrors errors){
+        validatePresence(errors, "movieId", movieId);
+        validatePresence(errors, "cinemaId", cinemaId);
+
+        if(dates == null && calendar == null){
+            errors.add("dates & calendar", "enter dates or calendar");
+        }
+
+        if(dates != null && calendar != null){
+            errors.add("dates & calendar", "enter dates or calendar, not both");
+        }
+
+        if(dates == null && dates.isEmpty()){
+            errors.add("dates", "dates can't be empty");
+        }
+
+        if (calendar != null){
+            validatePresence(errors,"calendar: fromDate", calendar.getFromDate());
+            validatePresence(errors, "calendar: untilDate", calendar.getUntilDate());
+
+            if (calendar.getHours() == null || calendar.getHours().isEmpty() ) {
+                errors.add("calendar: hours ", "hours set is empty or blank");
+            }
+            if (calendar.getWeekDays() == null || calendar.getWeekDays().isEmpty()) {
+                errors.add("calendar: weekDays ", "weekDays set is empty or blank");
+            }
+
+            validateCorrectnessWeekOfDays(errors, calendar.getWeekDays());
+        }
+    }
+
+    private void validateCorrectnessWeekOfDays(Validatable.ValidationErrors errors, Set<String> weekDays) {
+        if (weekDays != null && !weekDays.stream().allMatch(day -> validateDayOfWeek(day))){
+            errors.add("calendar: weekDays ", "weekDays names must be real");
+        }
+    }
+
+    private boolean validateDayOfWeek(String day) {
+        try {
+            DayOfWeek.valueOf(day.toUpperCase());
+        } catch (IllegalArgumentException e){
+            return false;
+        }
+        return true;
     }
 }
